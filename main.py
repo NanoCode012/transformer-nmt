271be4d5395d44a736266717ef88705c81b51c00
NanoCode012 <kevinvong@rocketmail.com>
2021-05-04 15:21:53 +0700

    Updated to use dataset

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ from utils import *
 from generator import *
 from critirion import *
 from optimizer import *
+from dataset import *
 
 use_cuda = True
 
-def make_model(src_vocab, tgt_vocab, N=6, 
-               d_model=512, d_ff=2048, h=8, dropout=0.1):
+
+def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
     "Helper: Construct a model from hyperparameters."
     c = copy.deepcopy
     attn = MultiHeadedAttention(h, d_model)
@@ -24,22 +25,23 @@ def make_model(src_vocab, tgt_vocab, N=6,
     position = PositionalEncoding(d_model, dropout)
     model = EncoderDecoder(
         Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
-        Decoder(DecoderLayer(d_model, c(attn), c(attn), 
-                             c(ff), dropout), N),
+        Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
         nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
         nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
-        Generator(d_model, tgt_vocab))
-    
-    # This was important from their code. 
+        Generator(d_model, tgt_vocab),
+    )
+
+    # This was important from their code.
     # Initialize parameters with Glorot / fan_avg.
     for p in model.parameters():
         if p.dim() > 1:
             nn.init.xavier_uniform_(p)
     return model
 
+
 def data_gen(V, batch, nbatches):
     "Generate random data for a src-tgt copy task."
-    global use_cuda 
+    global use_cuda
     for i in range(nbatches):
         data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
         data[:, 0] = 1
@@ -53,28 +55,22 @@ def data_gen(V, batch, nbatches):
 
         yield Batch(src, tgt, 0)
 
-class Batch:
-    "Object for holding a batch of data with mask during training."
-    def __init__(self, src, trg=None, pad=0):
-        self.src = src
-        self.src_mask = (src != pad).unsqueeze(-2)
-        if trg is not None:
-            self.trg = trg[:, :-1]
-            self.trg_y = trg[:, 1:]
-            self.trg_mask = \
-                self.make_std_mask(self.trg, pad)
 
-            if use_cuda:
-                self.trg_mask = self.trg_mask.cuda()
+max_src_in_batch, max_tgt_in_batch = None, None
 
-            self.ntokens = (self.trg_y != pad).data.sum()
-    
-    @staticmethod
-    def make_std_mask(tgt, pad):
-        "Create a mask to hide padding and future words."
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data)
-        return tgt_mask
+
+def batch_size_fn(new, count, sofar):
+    "Keep augmenting batch and calculate total number of tokens + padding."
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch, len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
+
 
 def run_epoch(data_iter, model, loss_compute):
     "Standard Training and Logging Function"
@@ -83,46 +79,79 @@ def run_epoch(data_iter, model, loss_compute):
     total_loss = 0
     tokens = 0
     for i, batch in enumerate(data_iter):
-        out = model.forward(batch.src, batch.trg, 
-                            batch.src_mask, batch.trg_mask)
+        out = model.forward(batch.src, batch.trg, batch.src_mask, batch.trg_mask)
         loss = loss_compute(out, batch.trg_y, batch.ntokens)
         total_loss += loss
         total_tokens += batch.ntokens
         tokens += batch.ntokens
         if i % 50 == 1:
             elapsed = time.time() - start
-            print("Epoch Step: %d Loss: %f Tokens per Sec: %f" %
-                    (i, loss / batch.ntokens, tokens / elapsed))
+            print(
+                "Epoch Step: %d Loss: %f Tokens per Sec: %f"
+                % (i, loss / batch.ntokens, tokens / elapsed)
+            )
             start = time.time()
             tokens = 0
         torch.cuda.empty_cache()
     return total_loss / total_tokens
 
+
 def main():
     global use_cuda
 
-    V = 11
-    
-    criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
-    model = make_model(V, V, N=2)
+    (SRC, TGT), (train, val, test) = load_data()
+    pad_idx = TGT.vocab.stoi["<blank>"]
+    BATCH_SIZE = 12000
+    train_iter = MyIterator(
+        train,
+        batch_size=BATCH_SIZE,
+        device=0,
+        repeat=False,
+        sort_key=lambda x: (len(x.src), len(x.trg)),
+        batch_size_fn=batch_size_fn,
+        train=True,
+    )
+    valid_iter = MyIterator(
+        val,
+        batch_size=BATCH_SIZE,
+        device=0,
+        repeat=False,
+        sort_key=lambda x: (len(x.src), len(x.trg)),
+        batch_size_fn=batch_size_fn,
+        train=False,
+    )
+
+    criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=0, smoothing=0.0)
+    model = make_model(len(SRC.vocab), len(TGT.vocab), N=2)
     print(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9)
-    
+
     if use_cuda:
         criterion = criterion.cuda()
         model = model.cuda()
 
-    model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400, optimizer)
+    model_opt = NoamOpt(
+        model.src_embed[0].d_model, factor=1, warmup=400, optimizer=optimizer
+    )
 
     for epoch in range(10):
-        print("Entering epoch : %d"%epoch)
+        print("Entering epoch : %d" % epoch)
         model.train()
-        run_epoch(data_gen(V, 30, 20), model, 
-                SimpleLossCompute(model.generator, criterion, model_opt))
+        run_epoch(
+            (rebatch(pad_idx, b) for b in train_iter),
+            model,
+            SimpleLossCompute(model.generator, criterion, model_opt),
+        )
         model.eval()
-        print(run_epoch(data_gen(V, 30, 5), model, 
-                        SimpleLossCompute(model.generator, criterion, None)))
+        print(
+            run_epoch(
+                (rebatch(pad_idx, b) for b in valid_iter),
+                model,
+                SimpleLossCompute(model.generator, criterion, None),
+            )
+        )
+
 
 if __name__ == "__main__":
-    print(torch.__version__) # 1.7+
+    print(torch.__version__)  # 1.7+
     main()
